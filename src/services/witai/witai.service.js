652187e92db5540049f4f532fa173dee0364bd39
FidFenix <fid.mamani@gmail.com>
2020-06-24 09:34:30 -0500

    First deploy on prod

--- src/services/witai/witai.service.js
+++ src/services/witai/witai.service.js
@@ -27,7 +27,7 @@ function speechApi(recordBlob, name) {
     const data = new FormData();
     data.append('audio', recordBlob, name);
 
-    return axios.post(`${config.localApi}/witai`, data, {
+    return axios.post(`${config.AWSWitaiUrl}/witai`, data, {
         'accept': 'application/json',
         'Accept-Language': 'en-US,en;q=0.8',
         'Content-Type': `multipart/form-data; boundary=${data._boundary}`
